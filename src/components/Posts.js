ad5bba4ddd3828aa21a1674cb14379f1e3ca993e
wanghao8080 <117663444@qq.com>
2018-01-03 17:32:09 +0800

    用 connect 创建 React 组件

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import { connect } from 'react-redux'
 
 const PostItem = ({ entity, onClickDeleteButton }) => {
   return (
@@ -24,43 +24,65 @@ const PostList = ({ entities, onClickDeleteButton }) => {
   )
 }
 
-class Posts extends Component {
-  static contextTypes = {
-    store: PropTypes.object
-  }
-
-  constructor (props, context) {
-    super(props)
-
-    console.log('Context：', context)
-    console.log('State：', context.store.getState())
-
-    this.store = context.store
-
-    this.unsubscribe = this.store.subscribe(() => this.forceUpdate())
-  }
-
-  componentWillUnMount () {
-    this.unsubscribe()
-  }
-
-  onClickDeleteButton = (id) => {
-    this.store.dispatch({
-      type: 'DELETE_POST',
-      id
-    })
-  }
-
-  render () {
-    const entities = this.store.getState().posts
-    return (
-      <PostList
-        entities={ entities }
-        onClickDeleteButton={ this.onClickDeleteButton }
-      />
-    )
+const mapStateToProps = (state) => {
+  return {
+    entities: state.posts
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickDeleteButton: (id) => {
+      dispatch({
+        type: 'DELETE_POST',
+        id
+      })
+    }
+  }
+}
+
+const Posts = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostList)
+
+// class Posts extends Component {
+//   static contextTypes = {
+//     store: PropTypes.object
+//   }
+//
+//   constructor (props, context) {
+//     super(props)
+//
+//     console.log('Context：', context)
+//     console.log('State：', context.store.getState())
+//
+//     this.store = context.store
+//
+//     this.unsubscribe = this.store.subscribe(() => this.forceUpdate())
+//   }
+//
+//   componentWillUnMount () {
+//     this.unsubscribe()
+//   }
+//
+//   onClickDeleteButton = (id) => {
+//     this.store.dispatch({
+//       type: 'DELETE_POST',
+//       id
+//     })
+//   }
+//
+//   render () {
+//     const entities = this.store.getState().posts
+//     return (
+//       <PostList
+//         entities={ entities }
+//         onClickDeleteButton={ this.onClickDeleteButton }
+//       />
+//     )
+//   }
+// }
 // Posts.contextTypes = {
 //   store: PropTypes.object
 // }
